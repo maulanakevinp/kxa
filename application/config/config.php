c60b4767adc61f52ea186e3bb0b45ea88b485251
Maulana Kevin Pradana <44237629+maulanakevinpradana@users.noreply.github.com>
2019-07-16 00:30:08 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') or exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/kxa/';
+$config['base_url'] = 'https://sampleku.azurewebsites.net/';
 
 /*
 |--------------------------------------------------------------------------
